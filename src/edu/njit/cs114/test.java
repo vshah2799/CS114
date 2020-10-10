4b62b417384b56bde43cad4d44ecd9766931680d
vshah <vshah2799@gmail.com>
2020-10-09 21:48:25 -0400

    HW 4 almost complete

--- src/edu/njit/cs114/test.java
+++ src/edu/njit/cs114/test.java
@@ -1,11 +1,27 @@
 package edu.njit.cs114;
-
-import java.util.Arrays;
+import java.util.Random;
 
 public class test {
 
     public static void main(String[] args){
-        
+            test test1 = new test();
+            System.out.println(test1.piFinder(1000000000));
+    }
+    public double piFinder(int num){
+        Random ran = new Random();
+        int numInCircle = 0;
+        int numNotInCircle = 0;
+        for(int i = 0; i<=num; i++){
+            double x = ran.nextDouble();
+            double y = ran.nextDouble();
+            if((x*x+y*y)<=1){
+                numInCircle++;
+            }
+            numNotInCircle++;
+        }
+
+        return 4.0*numInCircle/numNotInCircle;
+
     }
 
 }
